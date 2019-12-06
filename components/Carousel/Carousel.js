49c67776e7369b232fd875acc932c217f0b65efa
Jose <Jrivera6869@yahoo.com>
2019-12-06 14:05:14 -0500

    working on stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,41 @@
     <div class="right-button"> > </div>
   </div>
 */
+function Carousel(data){
+let carouseldiv = document.createElement(`div`)
+  carouseldiv.classList.add(`carousel`)
+
+let lftbtn = document.createElement(`div`)
+    lftbtn.classList.add(`left-button`)
+
+let imgone = document.createElement(`img`)
+    imgone.src = `./assets/carousel/mountains.jpeg`
+let imgtwo = document.createElement(`img`)
+    imgtwo.src = `./assets/carousel/computer.jpeg`
+let imgthree = document.createElement(`img`)
+    imgthree.src = `./assets/carousel/trees.jpeg`
+let imgfour = document.createElement(`img`)
+    imgfour.src = `./assets/carousel/turntable.jpeg`
+// let imgfive = document.createElement(`img`)
+//     imgfive.src = `./assets/carousel/turntable.jpeg`
+
+let rghtbtn = document.createElement(`div`)
+    rghtbtn.classList.add(`right-button`)
+
+    // carouseldiv.append(lftbtn,imgone,imgtwo,imgthree,imgfour)
+    carouseldiv.appendChild(lftbtn)
+    carouseldiv.appendChild(imgone)
+    carouseldiv.appendChild(imgtwo)
+    carouseldiv.appendChild(imgthree)
+    carouseldiv.appendChild(imgfour)
+    // carouseldiv.appendChild(imgfive)
+    carouseldiv.appendChild(rghtbtn)
+
+    
+    return carouseldiv
+
+
+}
+
+let carouselentry = document.querySelector(`.carousel-container`)
+    carouselentry.appendChild(Carousel(data))
